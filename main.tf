2efa02fe4f9cbfeea7e9934bfc25e891048a38ac
Jim Andreasen <jim.andreasen@reifnir.com>
2021-03-07 23:54:43 -0500

    start of module

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
-# Terraform module structure expects entrypoint to be in root of the repo
+terraform {
+  backend "http" {
+    # Relies on environment variables for configuration
+  }
+  # Only testing with 0.14 for now
+  required_version = "~> 0.14"
+  required_providers {
+    azurerm = {
+      # Only testing with azurerm provider 2, need to test before being used in 3
+      version = "~> 2"
+    }
+  }
+}
