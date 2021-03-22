bd6b1548f3d15a2cf28dffd79a992dc77899a62e
Jim Andreasen <jim.andreasen@reifnir.com>
2021-03-22 03:41:51 -0400

    separate the simple example from the project we'll be using for CI/CD

---
+++ examples/ci-testing/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-common-storage"
+    storage_account_name = "sareifnircommonstorage"
+    container_name       = "terraform-state"
+    key                  = "terraform-azurerm-azure-functions-static-site-ci-testing.tfstate"
+  }
+}
+
+resource "random_string" "name_suffix" {
+  lower   = true
+  number  = true
+  upper   = false
+  special = false
+  length  = 8
+}
+
+module "ci_static_site" {
+  source                   = "../../"
+  name                     = "simple-example-static-site-${random_string.name_suffix.result}"
+  static_content_directory = "${path.root}/static-content"
+  tags = {
+    "Application" = "Simple example static site"
+    "ManagedBy"   = "Terraform"
+  }
+}
+
+output "debug" {
+  value = module.simple_example_static_site
+}
