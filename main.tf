93d79ee573ea640da0ae010826606029aef51fde
Jim Andreasen <jim.andreasen@reifnir.com>
2021-07-27 11:05:00 -0400

    fixed typo in variable name

--- main.tf
+++ main.tf
@@ -20,9 +20,9 @@ locals {
   #   contain numbers and lowercase letters
   #   be from 3 to 24 characters long ("sa" + max of 14 characters from name + 8 random == 24)
   #   must be unique across all storage accounts as they are given a unique public DNS name
-  storave_account_name_slug = substr(lower(local.name_without_special_char), 0, 14)
+  storage_account_name_slug = substr(lower(local.name_without_special_char), 0, 14)
 
-  storage_account_name            = "sa${local.storave_account_name_slug}${random_string.storage_account_name.result}"
+  storage_account_name            = "sa${local.storage_account_name_slug}${random_string.storage_account_name.result}"
   function_package_container_name = "${var.name}-static-site-az-fn-packages"
 
   now          = timestamp()
